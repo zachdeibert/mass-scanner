b02bdcafaefd67b30e459357570ce03c823b1bf6
Zach Deibert <zachdeibert@gmail.com>
2020-05-29 02:32:32 -0500

    Tested querying the image luminance

--- app/src/c/main.c
+++ app/src/c/main.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include "errors.h"
 #include "state.h"
 
@@ -6,8 +7,20 @@ error_t process_init(state_t *state) {
 }
 
 error_t process_main(state_t *state, image_t *image, bitmap_t *bitmap, augment_t *augment) {
-    size_t dx = image->width / 10;
-    size_t dy = image->height / 10;
+    uint64_t total_luminance = 0;
+    uint8_t *col = image->y.buf;
+    for (int y = 0; y < image->height; ++y) {
+        uint8_t *row = col;
+        col += image->y.row_stride;
+        for (int x = 0; x < image->width; ++x) {
+            total_luminance += *row;
+            row += image->y.pixel_stride;
+        }
+    }
+    uint64_t max_luminance = 255L * ((uint64_t) image->width) * ((uint64_t) image->height);
+    float luminance = ((float) total_luminance) / ((float) max_luminance);
+    size_t dx = (size_t) (image->width * luminance);
+    size_t dy = (size_t) (image->height * luminance);
     augment->valid = 1;
     augment->points[0].x = dx;
     augment->points[0].y = dy;
